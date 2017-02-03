a9825aaa51d358b525e3cc4f453255aef7c73af2
Greg Corradini <gregcorradini@gmail.com>
2017-02-02 17:49:28 -0800

    modify answerset

--- Module4/module4_lab2_min_max_med_mean.py
+++ Module4/module4_lab2_min_max_med_mean.py
@@ -7,7 +7,7 @@ ABSOLUTE_TSV_PATH=os.path.join( os.path.split(os.path.abspath(__name__))[0], 'da
 def get_max_width(table, padding=2):
     #calculate the max width of the table cell
     max_width = 0
-    for row in table.get('rows',[]):
+    for row in table[1:]: # skip the header row:
         new_max = max([len(value) for value in row]) + padding
         # only set the new max if it's actually greater
         if new_max > max_width:
@@ -16,21 +16,20 @@ def get_max_width(table, padding=2):
 
 def print_table(table):
     max_width = get_max_width(table)
-    print(*[str(i).center(max_width) for i in table.get('header',[])], sep='|')
-    for row in table.get('rows',[]):
+    for row in table:
         print(*[str(i).center(max_width) for i in row], sep='|')
 
 def calc_max_temp_average(table):
     max_temps = []
-    for row in in_memory_table.get('rows', []):
-        max_column = len(row) - 1 
+    for row in table[1:]: # skip the header row:
+        max_column = len(row) - 1
         max_temps.append(int(row[max_column])) 
 
     print("[ AVERAGE MAX TEMP ]: {}".format(sum(max_temps)/len(max_temps)))
 
 def calc_min_temp(table):
     min = None
-    for row in in_memory_table.get('rows', []):
+    for row in table[1:]: # skip the header row:
         min_column = len(row) - 2
         new_min = int(row[min_column]) 
 
@@ -44,7 +43,7 @@ def calc_min_temp(table):
 
 def calc_max_temp(table):
     max_temps = []
-    for row in in_memory_table.get('rows', []):
+    for row in table[1:]: # skip the header row:
         max_column = len(row) - 1
         max_temps.append( int(row[max_column]) )
 
@@ -53,25 +52,19 @@ def calc_max_temp(table):
 def calc_median_temp(table):
     import statistics
     all_temps = []
-    for row in in_memory_table.get('rows', []):
+    for row in table[1:]: # skip the header row:
         min_column = len(row) - 2
         max_column = len(row) - 1
         all_temps = all_temps + [ int(row[min_column]), int(row[max_column]) ]
 
     print("[ MEDIAN OF ALL TEMPS ]: {}".format(statistics.median(all_temps)))
 
-in_memory_table = {
-    'header': [],
-    'rows': []
-}
+in_memory_table = []
 with open( ABSOLUTE_TSV_PATH, 'r' ) as file_sock:
     for line in file_sock:
         line = line.strip() # strip off extraneous whitespace chars such as newline or tabs
         row = line.split('\t')
-        if len(row) > 0 and row[0].startswith('S'):
-            in_memory_table['header'] = row
-        else:
-            in_memory_table['rows'].append(row)
+        in_memory_table.append(row)
 
 # RUN THIS THANG
 print_table(in_memory_table)
